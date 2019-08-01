952418cc351c270514f824b541d7ca222131753b
Faisal2904 <ahmad.irfan94@gmail.com>
2019-08-01 12:32:51 +0530

    1 file modified

--- src/app/app-highlight/app-highlight.directive.ts
+++ src/app/app-highlight/app-highlight.directive.ts
@@ -1,13 +1,21 @@
-import {Directive,ElementRef,OnInit} from '@angular/core';
+import {Directive,ElementRef,OnInit,Renderer2,HostListener} from '@angular/core';
 @Directive({
   selector:'[appHighlight]'
 })
 export class AppHighlightDirective implements OnInit{
-  constructor(private elementRef:ElementRef){
+  constructor(private elementRef:ElementRef,private renderer:Renderer2){
 
   }
   ngOnInit(){
-    this.elementRef.nativeElement.style.backgroundColor='green';
+    //this.elementRef.nativeElement.style.backgroundColor='green';
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','blue');
+  }
+  @HostListener('mouseenter') mouseOver(){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','blue');
+  }
+
+   @HostListener('mouseleave') mouseLeave(){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','transparent');
   }
 
 }
