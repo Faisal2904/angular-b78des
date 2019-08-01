97899093a9f5a036578206fce6dc3e7d350f879b
Faisal2904 <ahmad.irfan94@gmail.com>
2019-08-01 14:16:53 +0530

    2 files modified

--- src/app/app-highlight/app-highlight.directive.ts
+++ src/app/app-highlight/app-highlight.directive.ts
@@ -1,9 +1,13 @@
-import {Directive,ElementRef,OnInit,Renderer2,HostListener,HostBinding} from '@angular/core';
+import {Directive,ElementRef,OnInit,Renderer2,HostListener,HostBinding,Input} from '@angular/core';
 @Directive({
   selector:'[appHighlight]'
 })
 export class AppHighlightDirective implements OnInit{
-  @HostBinding('style.backgroundColor') backgrndColor:string;
+  @Input() dcolor:string='blue';
+  @Input() hcolor:string='transparent';
+
+
+  @HostBinding('style.backgroundColor') backgrndColor:string=this.hcolor;
   constructor(private elementRef:ElementRef,private renderer:Renderer2){
 
   }
@@ -12,13 +16,16 @@ export class AppHighlightDirective implements OnInit{
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','blue');
   }
   @HostListener('mouseenter') mouseOver(){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color',this.color);
+
+    this.backgrndColor=this.dcolor;
+
   }
 
    @HostListener('mouseleave') mouseLeave(){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color','transparent');
 
-    this.backgrndColor='transparent';
+    this.backgrndColor=this.hcolor;
   }
 
 }
