e8a2946aa5e4c34b5d5d375476c785c8ac5c76f6
Faisal2904 <ahmad.irfan94@gmail.com>
2019-08-05 18:52:19 +0530

    3 files modified

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -1,16 +1,21 @@
-import {Component} from '@angular/core';
+import {Component,ViewChild,OnInit,ElementRef} from '@angular/core';
 @Component({
   selector: 'app-assignment',
   templateUrl:'./assignment.component.html',
   styleUrls:['./assignment.component.css']
 })
 
-export class AssignmentComponent{
+export class AssignmentComponent implements OnInit{
   showStat=false;
   count=0;
   array=[];
+  @ViewChild('ref') xyz:ElementRef;
 
+  ngOnInit(){
+    console.log(this.xyz.nativeElement.value);
+  }
   onShow(){
+      console.log(this.xyz.nativeElement.value);
     this.count++;
     this.array.push(Date())
    // console.log(this.array);
